9e4a2a379d1117b9588bce04c4f4d762b376948e
XakimAA <malutok@mail.ru>
2020-04-22 15:34:44 +0500

    созданы страницы списка и карточки + роутинг

---
+++ src/pages/UserCard/usercard.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const UserCard = (props) => {
+  return <div>It's card</div>;
+};
+
+export default UserCard;
---
+++ src/pages/UserList/userlist.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const UserList = (props) => {
+  return <div>It's list</div>;
+};
+
+export default UserList;
