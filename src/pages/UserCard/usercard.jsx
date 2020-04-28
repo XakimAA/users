98282482f4a1ad3d090bd945df74e1f39be82f63
XakimAA <malutok@mail.ru>
2020-04-28 15:42:51 +0500

    Вывод ошибок на форме пользователя

--- src/pages/UserCard/usercard.jsx
+++ src/pages/UserCard/usercard.jsx
@@ -1,14 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Grid, TextField, Paper, Typography, FormControl } from '@material-ui/core';
-import { Table, Button, Collapse } from 'xsolla-uikit';
+import { Table, Button, Collapse, Loader } from 'xsolla-uikit';
 import moment from 'moment';
 
 import { Service } from '../../Service';
 const service = new Service();
 
 const columns = [
-  // { id: 'operation_id', name: 'ID операции', fieldGetter: 'operation_id' },
   { id: 'date', name: 'дата', fieldGetter: 'date' },
   { id: 'transaction_type', name: 'тип транзакции', fieldGetter: 'transaction_type' },
   { id: 'amount', name: 'сумма операции', fieldGetter: 'amount' },
@@ -38,31 +37,77 @@ const UserCard = (props) => {
     comment: '',
   });
 
+  const [errorFields, setErrorFields] = useState({
+    user_id: false,
+    user_name: false,
+    user_custom: false,
+    email: false,
+  });
+
   const [loadSave, setLoadSave] = useState(false);
+  const [loadingPage, setloadingPage] = useState(true);
+  const [userNotFound, setUserNotFound] = useState(true);
   const [transactions, setTransactions] = useState([]);
   const [addTransactionLoad, setAddTransactionLoad] = useState(false);
+  const [errorText, setErrorText] = useState('');
   useEffect(() => {
-    service.getUserInfo(id).then((user) => {
+    const getUserInfo = service.getUserInfo(id).then((user) => {
       setValues(user.data);
+      return user;
     });
-    service.getTransactions(id).then(({ data }) => {
+    const getTransactions = service.getTransactions(id).then(({ data }) => {
       setTransactions(data);
+      return data;
+    });
+    Promise.all([getUserInfo, getTransactions]).then((values) => {
+      setUserNotFound(values[0].data.http_status_code === 404)
+      setloadingPage(false);
     });
   }, []);
 
   const handlerOnChange = (inputID) => (event) => {
     setValues({ ...values, [inputID]: event.target.value });
+    setErrorFields({ ...errorFields, [inputID]: false });
   };
 
   const handlerSubmitClick = (event) => {
-    event.preventDefault();
+    // event.preventDefault();
+    setErrorText('');
     setLoadSave(true);
     service
       .updateUser(values)
       .then((answer) => {
         setLoadSave(false);
-        if (!answer.data && answer.statusText === 'OK' && answer.status === 200) {
-          console.log('добавлен');
+        if (answer.statusText === 'OK' && answer.status === 200) {
+          let error = '';
+          if (
+            !!answer.data &&
+            !!answer.data.extended_message &&
+            !!answer.data.extended_message.global_errors &&
+            answer.data.extended_message.global_errors.length > 0
+          )
+            error = answer.data.extended_message.global_errors.reduce(
+              (str, current) => str + ' ' + current,
+              0
+            );
+
+          if (
+            !!answer.data &&
+            !!answer.data.extended_message &&
+            !!answer.data.extended_message.property_errors
+          )
+            error +=
+              'Неверно заполнено поле:' +
+              Object.keys(answer.data.extended_message.property_errors).reduce((str, current) => {
+                setErrorFields({ ...errorFields, [current]: true });
+                return str + ' ' + current + '\n';
+              }, '');
+
+          if (error === '') console.log('добавлен');
+          else {
+            setErrorText(error);
+            console.log('ошибка', error);
+          }
         } else {
           console.log('ошибка', answer);
         }
@@ -83,7 +128,12 @@ const UserCard = (props) => {
       .addTransaction(transactionInfo)
       .then((answer) => {
         setAddTransactionLoad(false);
-        if (!!answer.data && !answer.data.message && answer.statusText === 'OK' && answer.status === 200) {
+        if (
+          !!answer.data &&
+          !answer.data.message &&
+          answer.statusText === 'OK' &&
+          answer.status === 200
+        ) {
           console.log('добавлен');
           service.getTransactions(id).then(({ data }) => {
             setTransactions(data);
@@ -100,185 +150,208 @@ const UserCard = (props) => {
 
   return (
     <div style={{ padding: '20px' }}>
-      <Paper style={{ padding: '20px', marginBottom: '20px' }}>
-        <Typography component="p" align="left" style={{ marginBottom: '20px' }}>
-          Информация о пользователе
-        </Typography>
-
-        <Grid container spacing={2}>
-          <Grid item xs={12} md={6}>
-            <FormControl fullWidth>
-              <TextField
-                id="user_id"
-                label="ID"
-                variant="outlined"
-                onChange={handlerOnChange('user_id')}
-                value={values.user_id}
-                disabled
-              />
-            </FormControl>
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <FormControl fullWidth>
-              <TextField
-                id="user_name"
-                label="Имя пользователя"
-                variant="outlined"
-                onChange={handlerOnChange('user_name')}
-                value={values.user_name}
-              />
-            </FormControl>
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <FormControl fullWidth>
-              <TextField
-                id="user_custom"
-                label="Полное имя"
-                variant="outlined"
-                onChange={handlerOnChange('user_custom')}
-                value={values.user_custom}
-              />
-            </FormControl>
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <FormControl fullWidth>
-              <TextField
-                id="email"
-                label="E-mail"
-                variant="outlined"
-                onChange={handlerOnChange('email')}
-                value={values.email}
-              />
-            </FormControl>
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <FormControl fullWidth>
-              <TextField
-                id="register_date"
-                label="Дата регистрации"
-                variant="outlined"
-                value={moment(values.register_date).format('DD.MM.YYYY')}
-                disabled
-              />
-            </FormControl>
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <FormControl fullWidth>
-              <TextField
-                id="balance"
-                label="Баланс"
-                variant="outlined"
-                value={values.balance}
-                disabled
-              />
-            </FormControl>
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <FormControl fullWidth>
-              <TextField
-                id="wallet_amount"
-                label="Кошелек"
-                variant="outlined"
-                value={values.wallet_amount}
-                disabled
-              />
-            </FormControl>
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <FormControl fullWidth>
-              <TextField
-                id="wallet_currency"
-                label="Валюта"
-                variant="outlined"
-                value={values.wallet_currency}
-                disabled
-              />
-            </FormControl>
-          </Grid>
-          <Grid item xs={12} align="right">
-            <Button
-              type="submit"
-              appearance="secondary"
-              onClick={handlerSubmitClick}
-              fetching={loadSave}
+      {loadingPage && <Loader color="blue" fullscreen={true} centered={true} />}
+      {!loadingPage && userNotFound && (
+        <div>Пользователя с таким id не существует</div>
+      )}
+      {!loadingPage && !userNotFound &&(
+        <>
+          <Paper style={{ padding: '20px', marginBottom: '20px' }}>
+            <Typography component="p" align="left" style={{ marginBottom: '20px' }}>
+              Информация о пользователе
+            </Typography>
+            <Grid container spacing={2}>
+              <Grid item xs={12} md={6}>
+                <FormControl fullWidth>
+                  <TextField
+                    id="user_id"
+                    label="ID"
+                    variant="outlined"
+                    error={errorFields.user_id}
+                    onChange={handlerOnChange('user_id')}
+                    value={values.user_id}
+                    disabled
+                  />
+                </FormControl>
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <FormControl fullWidth>
+                  <TextField
+                    id="user_name"
+                    label="Имя пользователя"
+                    variant="outlined"
+                    error={errorFields.user_name}
+                    onChange={handlerOnChange('user_name')}
+                    value={values.user_name}
+                  />
+                </FormControl>
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <FormControl fullWidth>
+                  <TextField
+                    id="user_custom"
+                    label="Полное имя"
+                    variant="outlined"
+                    error={errorFields.user_custom}
+                    onChange={handlerOnChange('user_custom')}
+                    value={values.user_custom}
+                  />
+                </FormControl>
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <FormControl fullWidth>
+                  <TextField
+                    id="email"
+                    label="E-mail"
+                    variant="outlined"
+                    error={errorFields.email}
+                    onChange={handlerOnChange('email')}
+                    value={values.email}
+                  />
+                </FormControl>
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <FormControl fullWidth>
+                  <TextField
+                    id="register_date"
+                    label="Дата регистрации"
+                    variant="outlined"
+                    value={moment(values.register_date).format('DD.MM.YYYY')}
+                    disabled
+                  />
+                </FormControl>
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <FormControl fullWidth>
+                  <TextField
+                    id="balance"
+                    label="Баланс"
+                    variant="outlined"
+                    value={values.balance}
+                    disabled
+                  />
+                </FormControl>
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <FormControl fullWidth>
+                  <TextField
+                    id="wallet_amount"
+                    label="Кошелек"
+                    variant="outlined"
+                    value={values.wallet_amount}
+                    disabled
+                  />
+                </FormControl>
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <FormControl fullWidth>
+                  <TextField
+                    id="wallet_currency"
+                    label="Валюта"
+                    variant="outlined"
+                    value={values.wallet_currency}
+                    disabled
+                  />
+                </FormControl>
+              </Grid>
+              <Grid item xs={12} md={10}>
+                <FormControl fullWidth>
+                  <Typography component="p" align="left" style={{ color: '#ff0000' }}>
+                    {errorText}
+                  </Typography>
+                </FormControl>
+              </Grid>
+              <Grid item xs={12} md={2} align="right">
+                <Button
+                  type="submit"
+                  appearance="secondary"
+                  onClick={handlerSubmitClick}
+                  fetching={loadSave}
+                >
+                  Изменить
+                </Button>
+              </Grid>
+            </Grid>
+          </Paper>
+          <Paper style={{ padding: '20px', marginBottom: '20px' }}>
+            <Collapse
+              isOpened={false}
+              staticElements={1}
+              collapsedLabel={() => `Ввести данные`}
+              expandedLabel={() => 'Спрятать'}
             >
-              Изменить
-            </Button>
-          </Grid>
-        </Grid>
-      </Paper>
-      <Paper style={{ padding: '20px', marginBottom: '20px' }}>
-        <Collapse
-          isOpened={false}
-          staticElements={1}
-          collapsedLabel={() => `Ввести данные`}
-          expandedLabel={() => 'Спрятать'}
-        >
-          <Typography component="p" align="left" style={{ marginBottom: '20px' }}>
-            Добавить операцию
-          </Typography>
-          <Grid container spacing={2}>
-            <Grid item xs={12} md={5}>
-              <FormControl fullWidth>
-                <TextField
-                  id="amount"
-                  label="Сумма"
-                  variant="outlined"
-                  onChange={handlerOnChangeTransaction('amount')}
-                  value={transactionInfo.amount}
-                  type="number"
-                />
-              </FormControl>
-            </Grid>
-            <Grid item xs={12} md={5}>
-              <FormControl fullWidth>
-                <TextField
-                  id="comment"
-                  label="Комментарий"
-                  variant="outlined"
-                  onChange={handlerOnChangeTransaction('comment')}
-                  value={transactionInfo.comment}
-                />
-              </FormControl>
-            </Grid>
-            <Grid item xs={12} md={2} align="right">
-              <Button
-                type="submit"
-                appearance="secondary"
-                onClick={handlerAddTransation}
-                fetching={addTransactionLoad}
-                style={{ height: '100%' }}
-              >
-                +
-              </Button>
-            </Grid>
-          </Grid>
-        </Collapse>
-      </Paper>
-      <Table
-        columns={columns}
-        rows={transactions}
-        compact={true}
-        className="table-wrapper"
-        tableClassName="user-table"
-        renderEmptyMessage={() => <div>Нет данных</div>}
-        renderRow={(data) => {
-          data.row['date'] = moment(data.row['date']).format('DD.MM.YYYY');
-          return (
-            <tr className={data.className} key={data.row.operation_id}>
-              {data.columns.map((column, index) => (
-                <data.CellComponent
-                  key={index}
-                  column={column}
-                  row={data.row}
-                  rowIndex={data.rowIndex}
-                  columnIndex={index}
-                />
-              ))}
-            </tr>
-          );
-        }}
-      />
+              <Typography component="p" align="left" style={{ marginBottom: '20px' }}>
+                Добавить операцию
+              </Typography>
+              <Grid container spacing={2}>
+                <Grid item xs={12} md={5}>
+                  <FormControl fullWidth>
+                    <TextField
+                      id="amount"
+                      label="Сумма"
+                      variant="outlined"
+                      onChange={handlerOnChangeTransaction('amount')}
+                      value={transactionInfo.amount}
+                      type="number"
+                    />
+                  </FormControl>
+                </Grid>
+                <Grid item xs={12} md={5}>
+                  <FormControl fullWidth>
+                    <TextField
+                      id="comment"
+                      label="Комментарий"
+                      variant="outlined"
+                      onChange={handlerOnChangeTransaction('comment')}
+                      value={transactionInfo.comment}
+                    />
+                  </FormControl>
+                </Grid>
+                <Grid item xs={12} md={2} align="right">
+                  <Button
+                    type="submit"
+                    appearance="secondary"
+                    onClick={handlerAddTransation}
+                    fetching={addTransactionLoad}
+                    style={{ height: '100%' }}
+                  >
+                    +
+                  </Button>
+                </Grid>
+              </Grid>
+            </Collapse>
+          </Paper>
+          <Paper style={{ marginBottom: '20px' }}>
+            <Typography component="p" align="left" style={{ padding: '20px' }}>
+              Все операции пользователя
+            </Typography>
+            <Table
+              columns={columns}
+              rows={transactions}
+              compact={true}
+              className="table-wrapper"
+              tableClassName="user-table"
+              renderEmptyMessage={() => <div>Нет данных</div>}
+              renderRow={(data) => {
+                data.row['date'] = moment(data.row['date']).format('DD.MM.YYYY');
+                return (
+                  <tr className={data.className} key={data.row.operation_id}>
+                    {data.columns.map((column, index) => (
+                      <data.CellComponent
+                        key={index}
+                        column={column}
+                        row={data.row}
+                        rowIndex={data.rowIndex}
+                        columnIndex={index}
+                      />
+                    ))}
+                  </tr>
+                );
+              }}
+            />
+          </Paper>
+        </>
+      )}
     </div>
   );
 };
