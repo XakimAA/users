1b3749de6c569279b0b279583eb018d6d58087a8
XakimAA <malutok@mail.ru>
2020-04-28 20:35:13 +0500

    update readme.md

--- src/Service.js
+++ src/Service.js
@@ -14,14 +14,23 @@ export class Service {
   }
 
   updateUser(user) {
-    return axios.put(`${axios.defaults.baseURL}/users/${user.user_id}`,user);
+    return axios.put(`${axios.defaults.baseURL}/users/${user.user_id}`, user);
   }
 
-  getTransactions(userID,date_from="1970-12-28T15%3A00%3A00Z",date_to="2020-12-28T15%3A10%3A00Z"){
-    return axios.get(`${axios.defaults.baseURL}/users/${userID}/transactions?datetime_from=${date_from}&datetime_to=${date_to}`);
+  getTransactions(
+    userID,
+    date_from = '1970-12-28T15%3A00%3A00Z',
+    date_to = '2200-12-28T15%3A10%3A00Z'
+  ) {
+    return axios.get(
+      `${axios.defaults.baseURL}/users/${userID}/transactions?datetime_from=${date_from}&datetime_to=${date_to}`
+    );
   }
 
-  addTransaction(transaction){
-    return axios.post(`${axios.defaults.baseURL}/users/${transaction.user_id}/recharge`,transaction);
+  addTransaction(transaction) {
+    return axios.post(
+      `${axios.defaults.baseURL}/users/${transaction.user_id}/recharge`,
+      transaction
+    );
   }
 }
